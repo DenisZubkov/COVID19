eb3cf9fbfcb223f9b8dcca3d97008d0b47a100ce
Denis Zubkov <denzu@mac.com>
2020-04-05 12:05:22 +0300

    Initial Commit

---
+++ COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by Dennis Zubkoff on 05.04.2020.
+//  Copyright © 2020 Denis Zubkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
